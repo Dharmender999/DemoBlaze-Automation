72606b5ab7b69bf1730c881012718fb3b61da214
Dharmender <dharmendernehra191919@gmail.com>
2025-09-06 11:34:41 +0530

    Added AuthModals

--- src/main/java/base/DriverFactory.java
+++ src/main/java/base/DriverFactory.java
@@ -1,5 +1,37 @@
 package base;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 public class DriverFactory {
 
+    private static final ThreadLocal<WebDriver> tlDriver = new ThreadLocal<>();
+
+    public static void initDriver() {
+        if (tlDriver.get() == null) {
+            WebDriverManager.chromedriver().setup();
+            ChromeOptions options = new ChromeOptions();
+            if (Boolean.parseBoolean(System.getProperty("headless", "false"))) {
+                options.addArguments("--headless=new");
+            }
+            options.addArguments("--disable-gpu");
+            options.addArguments("--window-size=1536,960");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            tlDriver.set(new ChromeDriver(options));
+        }
+    }
+
+    public static WebDriver getDriver() {
+        return tlDriver.get();
+    }
+
+    public static void quitDriver() {
+        if (tlDriver.get() != null) {
+            tlDriver.get().quit();
+            tlDriver.remove();
+        }
+    }
 }
--- src/main/java/pages/AuthModals.java
+++ src/main/java/pages/AuthModals.java
@@ -1,5 +1,44 @@
 package pages;
 
-public class AuthModals {
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.*;
 
+import java.time.Duration;
+
+public class AuthModals {
+    private final WebDriver driver;
+    private final WebDriverWait wait;
+
+    private final By signupUsername = By.id("sign-username");
+    private final By signupPassword = By.id("sign-password");
+    private final By signupBtn = By.xpath("//button[text()='Sign up']");
+
+    private final By loginUsername = By.id("loginusername");
+    private final By loginPassword = By.id("loginpassword");
+    private final By loginBtn = By.xpath("//button[text()='Log in']");
+
+    private final By welcomeUser = By.id("nameofuser");
+
+    public AuthModals(WebDriver driver, WebDriverWait wait) {
+        this.driver = driver;
+        this.wait = wait;
+    }
+
+    public void signup(String username, String password) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(signupUsername)).sendKeys(username);
+        driver.findElement(signupPassword).sendKeys(password);
+        driver.findElement(signupBtn).click();
+    }
+
+    public void login(String username, String password) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(loginUsername)).sendKeys(username);
+        driver.findElement(loginPassword).sendKeys(password);
+        driver.findElement(loginBtn).click();
+    }
+
+    public String waitForWelcomeUser() {
+        return new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.visibilityOfElementLocated(welcomeUser))
+                .getText();
+    }
 }
